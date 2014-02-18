50a8cb0e5e4e5ba2215148699de0942b89278fac
Andrei Silchankau <andrei.silchankau@gmail.com>
2014-02-18 05:26:11 +0300

    Rpc server functions

--- src/amqp.erl
+++ src/amqp.erl
@@ -10,9 +10,9 @@
 
 connect(Host, Queue) ->
 	{ok, Connection} = amqp_connection:start(#amqp_params_network{host = Host}),
-    {ok, Channel} = amqp_connection:open_channel(Connection),
-    amqp_channel:call(Channel, #'queue.declare'{queue = Queue}),
-    {ok, Connection, Channel}.
+  {ok, Channel} = amqp_connection:open_channel(Connection),
+  amqp_channel:call(Channel, #'queue.declare'{queue = Queue}),
+  {ok, Connection, Channel}.
 
 basic_subscribe(Channel, Queue, HandlePid) ->
 	amqp_channel:subscribe(Channel, #'basic.consume'{queue = Queue}, HandlePid),
@@ -22,26 +22,38 @@ basic_handle(Channel, Message, State, HandleFunc) ->
 	case Message of
 		#'basic.consume_ok'{} ->
 			ok;
-        {#'basic.deliver'{delivery_tag = Tag}, #amqp_msg{payload = Body}} ->
-            case HandleFunc(erlang:binary_to_term(Body), State) of
-            	ok -> amqp_channel:cast(Channel, #'basic.ack'{delivery_tag = Tag});
+    {#'basic.deliver'{delivery_tag = Tag}, #amqp_msg{payload = Body}} ->
+      case HandleFunc(erlang:binary_to_term(Body), State) of
+        ok -> amqp_channel:cast(Channel, #'basic.ack'{delivery_tag = Tag});
             	_ -> noproc
-            end,
-            ok
-    end.
+      end,
+      ok
+  end.
 
 basic_send(Channel, RoutingKey, Message) ->
-    amqp_channel:cast(Channel,
-                      #'basic.publish'{
-                      exchange = <<"">>,
-                      routing_key = RoutingKey},
-                      #amqp_msg{payload = erlang:term_to_binary(Message)}),
-    ok.
+  amqp_channel:cast(Channel,
+    #'basic.publish'{
+      exchange = <<"">>,
+      routing_key = RoutingKey},
+      #amqp_msg{payload = erlang:term_to_binary(Message)}
+  ),
+  ok.
 
 disconnect(Connection, Channel) ->
 	ok = amqp_channel:close(Channel),
-    ok = amqp_connection:close(Connection),
-    ok.
+  ok = amqp_connection:close(Connection),
+  ok.
+
+start_rpc_server(Host, Queue, HandleFunc) ->
+  {ok, Connection} = amqp_connection:start(#amqp_params_network{host = Host}),
+  Server = amqp_rpc_server:start(Connection, Queue, HandleFunc),
+  {ok, Connection, Server}.
+
+stop_rpc_server(Connection, Server) ->
+  ok = amqp_rpc_server:stop(Server),
+  ok = amqp_connection:close(Connection),
+  ok.
+
 
 
 
